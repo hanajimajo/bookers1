a9a56c850ced6c2e864f1a0473387fb85f625289
hanajimajo <hanakun0318@icloud.com>
2021-03-15 06:17:52 +0000

    Commit3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,15 @@ class BooksController < ApplicationController
   end
 
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully created."
+    else
+     @books = Book.all
+     render :index
+    end
+
   end
 
   def show
@@ -27,9 +33,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book)
+    flash[:notice] = "Book was successfully updated."
+   else
+    render :edit
+   end
+
   end
 
   def destroy
@@ -42,6 +53,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 
 end
